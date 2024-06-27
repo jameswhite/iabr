d865ff8beb649bf5fefcc6658ed3d6cdc4301697
James S. White <james@jameswhite.org>
2024-06-26 20:48:03 -0400

    jesus these filenames

--- config/usr/share/nginx/perl/lib/hello.pm
+++ config/usr/share/nginx/perl/lib/hello.pm
@@ -4,7 +4,10 @@ package hello;
 use CAM::PDF;
 use Data::Dumper;
 use nginx;
+use URI::Encode qw( uri_encode );
+use URI::Escape::XS qw( uri_unescape );
 use warnings;
+
 
 sub handler {
     my $r = shift;
@@ -30,7 +33,9 @@ sub handler {
       $r->print("{\n  \"ppi\": 200,\n  \"data\": [\n            [\n");
       for my $page_number (0 .. $page_count-1) {
           my $current_page=$page_number;
-          $r->print("              {\"width\": 1275, \"height\": 1650, \"uri\": \"/iabr/${rel_file}?page=${current_page}\"}");
+	  my $img_uri=uri_encode("/iabr/${rel_file}?page=${current_page}");
+	  $img_uri=~s/#/%23/g;
+          $r->print("              {\"width\": 1275, \"height\": 1650, \"uri\": \"$img_uri\"}");
           if($current_page < $page_count - 1){
             $r->print(",\n");
           }else{
@@ -47,20 +52,25 @@ sub handler {
       open(my $in_fh, '<', "/var/cache/git/bookreader/_index.html") or die "Cannot open /var/cache/git/bookreader/_index.html for reading: $!";
       my $content = do { local $/; <$in_fh> };
       close($in_fh) or die "Cannot close $input_file: $!";
-      $content =~ s/index.js/\/iabr\/$rel_file?page=js/g;
+      my $js_uri=uri_encode("\/iabr\/$rel_file?page=js");
+      $js_uri=~s/#/%23/g;
+      $content =~ s/index.js/$js_uri/g;
       $r->send_http_header("text/html");
       $r->print($content);
     }elsif($page eq "js"){
       open(my $in_fh, '<', "/var/cache/git/bookreader/_index.js") or die "Cannot open /var/cache/git/bookreader/_index.js for reading: $!";
       my $content = do { local $/; <$in_fh> };
       close($in_fh) or die "Cannot close $input_file: $!";
-      $content =~ s/index.json/\/iabr\/$rel_file?page=json/g;
+      my $json_uri=uri_encode("\/iabr\/$rel_file?page=json");
+      $json_uri=~s/#/%23/g;
+      $content =~ s/index.json/$json_uri/g;
       $r->send_http_header("text/javascript");
       $r->print($content);
     }else{
     # Read the specific page from the PDF file
     # The native perlmagick stuff doesn't seem to be able to upscale, so using the shell
-    open(my $convert_fh, '-|', "convert -density 200 $pdf_file\[$page\] png:-") 
+    $r->log_error(0,"::::::::::::".quotemeta(uri_unescape($pdf_file))."::::::::::::::::\n");
+    open(my $convert_fh, '-|', "convert -density 200 ". quotemeta(uri_unescape($pdf_file)) ."\[$page\] png:-") 
       or die "Cannot open convert process: $!";
     $convert_fh->autoflush(1);
     $r->send_http_header("image/png");
